0098ea8252b7a0d7d2c7708b56d17632448d090c
Mert Sozen <mertcode0@gmail.com>
2024-02-08 13:58:20 +0100

    added fillables, made them all guarded

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
 
 return new class extends Migration
 {
@@ -14,9 +15,20 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable(); // new field
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // new fields down here
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('adress')->nullable();
+            $table->enum('role', ['admin', 'agent', 'user'])->default('user'); // admin, agent, user
+            $table->enum('status', ['active', 'inactive'])->default('active'); // admin, agent, user
+
+            $table->string('username')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
