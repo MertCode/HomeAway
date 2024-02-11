6cb5e81d7e0d647a7d59571452c2e1b279151915
Mert Sozen <mertcode0@gmail.com>
2024-02-11 20:48:12 +0100

    made logoout functionallity to destroy (invalidate session)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
         return view('admin.index');
-    } // End of AdminDashboard
+    } // end method
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } // end method
 }
