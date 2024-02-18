7433c9549e4eff311c1e6b0070cf2c7d0f53deae
Mert Sozen <mertcode0@gmail.com>
2024-02-18 16:35:26 +0100

    Add admin profile store route ('post method')

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 }); // end admin group  middleware
 
 // agent group Middleware
@@ -38,4 +40,3 @@ Route::middleware(['auth', 'role:agent'])->group(function () {
 }); // end  agent group middleware
 
 route::get('admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
-
